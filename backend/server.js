d92ced2757257b312f9ed6b0db866377110cf3c8
arshidh <arshidbinashraf714@gmail.com>
2025-04-25 02:27:21 +0530

    app cors problems

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,11 @@ app.use(session({
     cookie: { secure: process.env.NODE_ENV === "production", maxAge: 10 * 60 * 1000 } // 10 min
 }));
 
-
+app.get('/api/offer', async (req, res) => {
+    const response = await axios.get('https://overbridgenet.com/jsv8/offer');
+    res.json(response.data);
+  });
+  
 // Database Connection
 connectDB()
 
@@ -49,10 +53,12 @@ const reviewRouter = require('./routes/reviewRoutes');
 // Middlewares
 app.use(express.json({ limit: "5mb" })); 
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin:"https://e-commerce-project-flax.vercel.app",
-    credentials: true,
-}));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://e-commerce-project-flax.vercel.app'); // Specify your domain here
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use(cookieParser());
 // app.use((req, res, next) => {
 //     console.log(`Incoming Request: ${req.method} ${req.url}`);
